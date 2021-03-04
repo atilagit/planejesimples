d9f02b37e433a01655cd3cd6f15959241f61f4ef
Atila Silva <atila__7@hotmail.com>
2021-03-04 20:07:03 -0300

    Project created

---
+++ backend/src/main/java/com/atimat/planejesimples/PlanejesimplesApplication.java
@@ -0,0 +1,13 @@
+package com.atimat.planejesimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanejesimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanejesimplesApplication.class, args);
+	}
+
+}
