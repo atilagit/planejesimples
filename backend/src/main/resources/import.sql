80bafc66e42b10eed4156dbf3c7164babb156010
Atila Silva <atila__7@hotmail.com>
2021-03-05 21:22:04 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into tb_user (name, email, password) values ('Átila Izac','atila__7@hotmail.com','321');
+insert into tb_user (name, email, password) values ('Mateus Suave','mateus__7@gmail.com','123');
+insert into tb_user (name, email, password) values ('Teste de um nome um pouco maior','teste__7@hotmail.com','222');
