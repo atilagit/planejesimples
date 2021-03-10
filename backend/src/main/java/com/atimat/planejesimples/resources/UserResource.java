5e86d05bcd90e7dba891b24555944e6324e55799
Atila Silva <atila__7@hotmail.com>
2021-03-09 21:15:03 -0300

    UserDTO

--- backend/src/main/java/com/atimat/planejesimples/resources/UserResource.java
+++ backend/src/main/java/com/atimat/planejesimples/resources/UserResource.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.atimat.planejesimples.entities.User;
+import com.atimat.planejesimples.dto.UserDTO;
 import com.atimat.planejesimples.services.UserService;
 
 @RestController
@@ -20,14 +20,14 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
-		List<User> list = service.findAll();
+	public ResponseEntity<List<UserDTO>> findAll(){
+		List<UserDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
-		User obj = service.findById(id);
+	public ResponseEntity<UserDTO> findById(@PathVariable Long id){
+		UserDTO obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 }
--- backend/src/main/java/com/atimat/planejesimples/services/UserService.java
+++ backend/src/main/java/com/atimat/planejesimples/services/UserService.java
@@ -1,11 +1,13 @@
 package com.atimat.planejesimples.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.atimat.planejesimples.dto.UserDTO;
 import com.atimat.planejesimples.entities.User;
 import com.atimat.planejesimples.respositories.UserRepository;
 
@@ -16,12 +18,14 @@ public class UserService {
 	private UserRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<User> findAll(){
-		return repository.findAll();
+	public List<UserDTO> findAll(){
+		List<User> list = repository.findAll();
+		return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 	}
 	
 	@Transactional(readOnly = true)
-	public User findById(Long id){
-		return repository.findById(id).orElse(null);
+	public UserDTO findById(Long id){
+		User obj = repository.findById(id).orElse(null);
+		return new UserDTO(obj);
 	}
 }
