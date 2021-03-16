d0f91e306128d2f224f08f21d0cdc1e9ffd25fe6
Atila Silva <atila__7@hotmail.com>
2021-03-16 20:35:34 -0300

    POST Planning

--- backend/src/main/java/com/atimat/planejesimples/services/PlanningService.java
+++ backend/src/main/java/com/atimat/planejesimples/services/PlanningService.java
@@ -10,7 +10,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.atimat.planejesimples.dto.PlanningDTO;
 import com.atimat.planejesimples.entities.Planning;
+import com.atimat.planejesimples.entities.User;
 import com.atimat.planejesimples.respositories.PlanningRepository;
+import com.atimat.planejesimples.respositories.UserRepository;
 import com.atimat.planejesimples.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -18,6 +20,9 @@ public class PlanningService {
 	
 	@Autowired
 	private PlanningRepository repository;
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	@Transactional(readOnly = true)
 	public List<PlanningDTO> findAll(){
@@ -29,6 +34,14 @@ public class PlanningService {
 	public PlanningDTO findById(Long id) {
 		Optional<Planning> obj = repository.findById(id);
 		Planning entity = obj.orElseThrow(() -> new ResourceNotFoundException("Planejamento não encontrado"));
+		return new PlanningDTO(entity);
+	}
+
+	@Transactional
+	public PlanningDTO insert(PlanningDTO dto) {
+		User user = userRepository.getOne(dto.getUser().getId());
+		Planning entity = new Planning(null, dto.getInitialDate(), dto.getFinalDate(), dto.getExpectedEntry(), dto.getRealEntry(), user);
+		entity = repository.save(entity);
 		return new PlanningDTO(entity);
 	}
 }
