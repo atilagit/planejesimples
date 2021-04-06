0e52f220e675e1ad455f4f6c8f42d8e50921b9a9
Atila Silva <atila__7@hotmail.com>
2021-04-06 20:54:27 -0300

    Item class - part 2

--- backend/src/main/java/com/atimat/planejesimples/entities/Item.java
+++ backend/src/main/java/com/atimat/planejesimples/entities/Item.java
@@ -3,11 +3,25 @@ package com.atimat.planejesimples.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name = "tb_item")
 public class Item implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant dueDate;
 	private Double expectancy;
 	private Double reality;
