21c69e5ee18e438a83d42528c9ad27d683b284ce
Atila Silva <atila__7@hotmail.com>
2021-04-14 20:54:38 -0300

    ItemName Class x User Class

--- backend/src/main/java/com/atimat/planejesimples/entities/ItemName.java
+++ backend/src/main/java/com/atimat/planejesimples/entities/ItemName.java
@@ -1,46 +1,36 @@
 package com.atimat.planejesimples.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 @Entity
-@Table(name = "tb_user")
-public class User implements Serializable {
+@Table(name = "tb_itemName")
+public class ItemName implements Serializable{
 	private static final long serialVersionUID = 1L;
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	private String email;
-	private String password;
-
-	@JsonIgnore
-	@OneToMany(mappedBy = "user")
-	private List<Planning> plannings = new ArrayList<>();
 	
-	@JsonIgnore
-	@OneToMany(mappedBy = "user")
-	private List<ItemName> itemName = new ArrayList<>();
-
-	public User() {
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	public ItemName() {
 	}
 
-	public User(Long id, String name, String email, String password) {
+	public ItemName(Long id, String name) {
+		super();
 		this.id = id;
 		this.name = name;
-		this.email = email;
-		this.password = password;
 	}
 
 	public Long getId() {
@@ -59,26 +49,6 @@ public class User implements Serializable {
 		this.name = name;
 	}
 
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public List<Planning> getPlannings() {
-		return plannings;
-	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -95,7 +65,7 @@ public class User implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		User other = (User) obj;
+		ItemName other = (ItemName) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
